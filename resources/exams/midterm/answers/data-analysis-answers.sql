03243f5c05aacf3128637bebf2ef899d90b6dd01
MJ Rossetti <s2t2@users.noreply.github.com>
2015-10-30 14:09:58 -0400

    Update data-analysis-answers.sql
    adding a few queries for ms access users

--- resources/exams/midterm/answers/data-analysis-answers.sql
+++ resources/exams/midterm/answers/data-analysis-answers.sql
@@ -21,13 +21,36 @@ SELECT id
 FROM listeners
 WHERE email_address LIKE '*@gwu.edu*'; -- ms access dbms
 
+SELECT *
+FROM listeners
+WHERE email_address LIKE '*gwu.edu'; -- ms access dbms
+
+
 -- 2a. How long is the longest song?
 
 SELECT max(duration_milliseconds) FROM songs;
 
+SELECT duration_milliseconds
+FROM songs
+ORDER BY duration_milliseconds DESC
+LIMIT 1;
+
+SELECT TOP 1 duration_milliseconds
+FROM songs
+ORDER BY duration_milliseconds DESC; -- ms access
+
 -- 2b. How long is the shortest song?
 
 SELECT min(duration_milliseconds) FROM songs;
+
+SELECT duration_milliseconds
+FROM songs
+ORDER BY duration_milliseconds ASC
+LIMIT 1;
+
+SELECT TOP 1 duration_milliseconds
+FROM songs
+ORDER BY duration_milliseconds ASC; -- ms access
 
 -- 3a. How many songs have been played?
 
@@ -38,12 +61,26 @@ SELECT count(DISTINCT songs.id) AS song_count
 FROM songs
 JOIN plays on plays.song_id = songs.id;
 
+SELECT count(song_id) as song_count
+FROM (
+    SELECT distinct song_id
+    FROM plays
+); -- ms access 
+
 -- 3b. How many songs have NOT been played?
 
 SELECT count(DISTINCT songs.id) AS unplayed_song_count
 FROM songs
 LEFT JOIN plays on plays.song_id = songs.id
 WHERE plays.song_id IS NULL;
+
+SELECT count(song_id)
+FROM (
+    SELECT distinct s.id as song_id
+    FROM songs s
+    LEFT JOIN plays p on p.song_id = s.id
+    WHERE p.song_id IS NULL
+); -- ms access
 
 -- 4a. Which listeners have listened to at least one song?
 
@@ -97,6 +134,12 @@ SELECT
 FROM thumbs
 GROUP BY thumb_type;
 
+SELECT 
+  thumb_type
+  ,count(id) as thumb_count
+FROM thumbs
+GROUP BY thumb_type; -- ms access
+
 -- 6b. Which thumb button gets pressed most often?
 
 SELECT
@@ -108,11 +151,11 @@ ORDER BY button_press_count DESC
 LIMIT 1; -- open source dbms
 
 SELECT TOP 1
- thumb_type
- ,count(DISTINCT id) as button_press_count
+  thumb_type
+  ,count(id) as button_press_count
 FROM thumbs
 GROUP BY thumb_type
-ORDER BY count(DISTINCT id) DESC; -- ms access DBMS
+ORDER BY count(id) DESC; -- ms access DBMS
 
 -- 6c. Which thumb button was pressed most often between '2015-01-01' and '2015-03-15'?
 
@@ -127,11 +170,11 @@ LIMIT 1; -- open source dbms
 
 SELECT TOP 1
  thumb_type
- ,count(DISTINCT id) as button_press_count
+ ,count(id) as button_press_count
 FROM thumbs
 WHERE thumb_pressed_at BETWEEN '2015-01-01' AND '2015-03-15'  -- assumes thumb_pressed_at is a date/datetime/timestamp datatype
 GROUP BY thumb_type
-ORDER BY count(DISTINCT id) DESC; -- ms access DBMS
+ORDER BY count(id) DESC; -- ms access DBMS
 
 SELECT
  thumb_type
@@ -144,11 +187,11 @@ LIMIT 1; -- open source dbms
 
 SELECT TOP 1
  thumb_type
- ,count(DISTINCT id) as button_press_count
+ ,count(id) as button_press_count
 FROM thumbs
 WHERE thumb_pressed_at >= '2015-01-01' AND thumb_pressed_at <= '2015-03-15' -- assumes thumb_pressed_at is a date/datetime/timestamp datatype
 GROUP BY thumb_type
-ORDER BY count(DISTINCT id) DESC; -- ms access DBMS
+ORDER BY count(id) DESC; -- ms access DBMS
 
 -- 7a. How many songs have been skipped?
 
@@ -183,9 +226,9 @@ LIMIT 100; -- open source dbms
 
 SELECT TOP 100
   songs.id
-  ,count(DISTINCT skips.id) as skip_count
+  ,count(skips.id) as skip_count
 FROM songs
 JOIN plays on plays.song_id = songs.id
 JOIN skips on skips.play_id = plays.id
 GROUP BY songs.id
-ORDER BY skip_count DESC; -- ms access dbms
+ORDER BY count(skips.id) DESC; -- ms access dbms
